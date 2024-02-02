36f98fdb19a3a0f80d0c4f81f71709ca90ce845e
cameron turner <17177161+cturner8@users.noreply.github.com>
2024-02-02 11:13:37 +0000

    prettier/eslint updates

--- web/.eslintrc.cjs
+++ web/.eslintrc.cjs
@@ -1,13 +1,14 @@
 module.exports = {
   root: true,
-  plugins: ["vue", "@typescript-eslint"],
+  plugins: ["vue", "@typescript-eslint", "prettier", "html"],
   extends: [
+    "plugin:prettier/recommended",
     "eslint:recommended",
     "plugin:vue/vue3-strongly-recommended",
-    "plugin:@typescript-eslint/recommended",
-    // "plugin:@typescript-eslint/recommended-type-checked",
-    // "plugin:@typescript-eslint/stylistic-type-checked",
-    // "plugin:@typescript-eslint/strict",
+    // "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:@typescript-eslint/stylistic-type-checked",
+    "plugin:@typescript-eslint/strict",
   ],
   env: {
     browser: true,
@@ -15,11 +16,18 @@ module.exports = {
     node: true,
   },
   rules: {
+    "prettier/prettier": "error",
     "no-console": "error",
+    "vue/multi-word-component-names": "off",
+    "vue/max-attributes-per-line": "off",
   },
+  parser: "vue-eslint-parser",
   parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ["./tsconfig.json", "./tsconfig.node.json"],
     parser: "@typescript-eslint/parser",
     ecmaVersion: "latest",
     sourceType: "module",
+    extraFileExtensions: [".vue"],
   },
 };
